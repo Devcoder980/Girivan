e93b9e858c435cbc5c714b5029a87a0f7d6e9ac3
mitava-shah <mitvahshah792@gmail.com>
2023-07-30 21:59:10 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
-import './App.css';
-import IntroSection from './Component/LandingPage/IntroSection';
-import Navbar from './Component/LandingPage/Navbar';
-
+import "./App.css";
+import IntroSection from "./Component/LandingPage/IntroSection";
+import Navbar from "./Component/LandingPage/Navbar";
+import Testimonial from "./Component/Testimonial";
 function App() {
-  return (
-<<<<<<< HEAD
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-         
-        </a>
-      </header>
-    </div>
-=======
-    <>
-      <Navbar />
-      <IntroSection/>
-    </>
->>>>>>> 671f90db9c1a375c6bac78ec64000def2fe774bc
-  );
+    return ( 
+        <>
+         {/* <Navbar/>
+        <IntroSection/> */}
+        <Testimonial/>
+        </>
+    );
 }
 
 export default App;
---
+++ src/Component/Testimonial.jsx
@@ -0,0 +1,139 @@
+import React from 'react'
+
+const Testimonial = () => {
+        return (
+        <>
+            <section id="-4" className="content-block image-grid bg-white text-center flex justify-center items-center h-full w-full">
+            <div className="container py-28">
+                <div className="limit-width-md ">
+                    <span className="preheading text-4xl text-center py-12 text-green-800" font-apple >
+                        Discover the essence of Africa
+                    </span>
+                    <h2 className="intro__heading uppercase text-[55px] font-medium mb-8 tracking-[8px] text-center text-black">Iconic Destinations</h2>
+                    <div className="mb-30 mb-md-70 text-black mx-36 mb-16">
+                        <p>Wildlife and wilderness. Safari and sundowners. Culture and community. Africa thrills with endless possibilities. Browse some of the continent’s most loved locations to whet your appetite for a tailormade dream trip that reveals the true heart of Africa.</p>
+                    </div>
+                </div>
+                <div className=" flex justify-center gap-8 flex-wrap flex-row">
+                    <div className="col-6 col-md-4">
+                        <a href="https://www.mahlatini.com/south-africa/cape-town" className="image-grid__card">
+                            <figure> 
+                                <div className="nooverflow h-96 w-96">
+                                    <picture><source type="image/webp" srcset="https://cdn.mahlatini.com/_900x1080_crop_center%20center_100_none/176173/iconic-destination-cape-town-card.webp" media="(min-width: 768px)"/>
+                                        <source type="image/jpg" srcset="https://cdn.mahlatini.cfoom/_900x1080_crop_center%20center_100_none/iconic-destination-cape-town-card.jpg" media="(min-width: 768px)"/>
+                                        <source type="image/webp" srcset="https://cdn.mahlatini.com/_302x362_crop_center%20center_100_none/176173/iconic-destination-cape-town-card.webp" media="(min-width: 1px)"/>
+                                        <source type="image/jpg" srcset="https://cdn.mahlatini.com/_302x362_crop_center%20center_100_none/iconic-destination-cape-town-card.jpg" media="(min-width: 1px)"/>
+                                        <img src="https://cdn.mahlatini.com/_900x1080_crop_center%20center_100_none/iconic-destination-cape-town-card.jpg" width="900" height="1080" alt="Iconic destination cape town card" loading="lazy"/>
+                                    </picture>
+                                </div>
+                                <div className="image-grid__card-title">
+                                    <h3>CAPE TOWN</h3>
+                                    <strong></strong>
+                                </div>
+                            </figure>
+                        </a>
+                    </div>
+                    <div className="col-6 col-md-4">
+                        <a href="https://www.mahlatini.com/south-africa/greater-kruger" className="image-grid__card">
+                            <figure>
+                                <div className="nooverflow h-96 w-96">
+                                    <picture><source type="image/webp" srcset="https://cdn.mahlatini.com/_900x1080_crop_center%20center_100_none/176073/kruger-national-park-card-iconic-des-Kirkmans-Kamp.webp" media="(min-width: 768px)"/>
+                                        <source type="image/jpg" srcset="https://cdn.mahlatini.com/_900x1080_crop_center%20center_100_none/kruger-national-park-card-iconic-des-Kirkmans-Kamp.jpg" media="(min-width: 768px)"/>
+                                        <source type="image/webp" srcset="https://cdn.mahlatini.com/_302x362_crop_center%20center_100_none/176073/kruger-national-park-card-iconic-des-Kirkmans-Kamp.webp" media="(min-width: 1px)"/>
+                                        <source type="image/jpg" srcset="https://cdn.mahlatini.com/_302x362_crop_center%20center_100_none/kruger-national-park-card-iconic-des-Kirkmans-Kamp.jpg" media="(min-width: 1px)"/>
+                                        <img src="https://cdn.mahlatini.com/_900x1080_crop_center%20center_100_none/kruger-national-park-card-iconic-des-Kirkmans-Kamp.jpg" width="900" height="1080" alt="Kruger national park card iconic des Kirkmans Kamp" loading="lazy"/>
+                                    </picture>
+                                </div>
+                                <div className="image-grid__card-title">
+                                    <h3>GREATER KRUGER</h3>
+                                    <strong></strong>
+                                </div>
+                            </figure>
+                        </a>
+                    </div>
+                    <div className="col-6 col-md-4">
+                        <a href="https://www.mahlatini.com/kenya/maasai-mara" className="image-grid__card">
+                            <figure>
+                                <div className="nooverflow h-96 w-96">
+                                    <picture>
+                                        <source type="image/webp" srcset="https://cdn.mahlatini.com/_900x1080_crop_center%20center_100_none/155927/Masai-Mara-Card.webp" media="(min-width: 768px)"/>
+                                        <source type="image/jpg" srcset="https://cdn.mahlatini.com/_900x1080_crop_center%20center_100_none/Masai-Mara-Card.jpg" media="(min-width: 768px)"/>
+                                        <source type="image/webp" srcset="https://cdn.mahlatini.com/_302x362_crop_center%20center_100_none/155927/Masai-Mara-Card.webp" media="(min-width: 1px)"/>
+                                        <source type="image/jpg" srcset="https://cdn.mahlatini.com/_302x362_crop_center%20center_100_none/Masai-Mara-Card.jpg" media="(min-width: 1px)"/>
+                                        <img src="https://cdn.mahlatini.com/_900x1080_crop_center%20center_100_none/Masai-Mara-Card.jpg" width="900" height="1080" alt="Masai Mara Card" loading="lazy"/>
+                                    </picture>
+                                </div>
+                                <div className="image-grid__card-title">
+                                    <h3>MAASAI MARA</h3>
+                                    <strong></strong>
+                                </div>
+                            </figure>
+                        </a>
+                    </div>
+                    <div className="col-6 col-md-4">
+                        <a href="https://www.mahlatini.com/botswana/okavango-delta" className="image-grid__card">
+                            <figure>
+                                <div className="nooverflow h-96 w-96">
+                                    <picture><source type="image/webp" srcset="https://cdn.mahlatini.com/_900x1080_crop_center%20center_100_none/155953/Okavango-Delta-Card.webp" media="(min-width: 768px)"/>
+                                        <source type="image/jpg" srcset="https://cdn.mahlatini.com/_900x1080_crop_center%20center_100_none/Okavango-Delta-Card.jpg" media="(min-width: 768px)"/>
+                                        <source type="image/webp" srcset="https://cdn.mahlatini.com/_302x362_crop_center%20center_100_none/155953/Okavango-Delta-Card.webp" media="(min-width: 1px)"/>
+                                        <source type="image/jpg" srcset="https://cdn.mahlatini.com/_302x362_crop_center%20center_100_none/Okavango-Delta-Card.jpg" media="(min-width: 1px)"/>
+                                        <img src="https://cdn.mahlatini.com/_900x1080_crop_center%20center_100_none/Okavango-Delta-Card.jpg" width="900" height="1080" alt="Okavango Delta Card" loading="lazy"/>
+                                    </picture>
+                                </div>
+                                <div className="image-grid__card-title">
+                                    <h3>OKAVANGO DELTA</h3>
+                                    <strong></strong>
+                                </div>
+                            </figure>
+                        </a>
+                    </div>
+                    <div className="col-6 col-md-4">
+                        <a href="https://www.mahlatini.com/tanzania/serengeti-and-grumeti-reserves" className="image-grid__card">
+                            <figure>
+                                <div className="nooverflow h-96 w-96">
+                                    <picture><source type="image/webp" srcset="https://cdn.mahlatini.com/_900x1080_crop_center%20center_100_none/155952/Serengeti-Grumeti-Reserves-Card.webp" media="(min-width: 768px)"/>
+                                        <source type="image/jpg" srcset="https://cdn.mahlatini.com/_900x1080_crop_center%20center_100_none/Serengeti-Grumeti-Reserves-Card.jpg" media="(min-width: 768px)"/>
+                                        <source type="image/webp" srcset="https://cdn.mahlatini.com/_302x362_crop_center%20center_100_none/155952/Serengeti-Grumeti-Reserves-Card.webp" media="(min-width: 1px)"/>
+                                        <source type="image/jpg" srcset="https://cdn.mahlatini.com/_302x362_crop_center%20center_100_none/Serengeti-Grumeti-Reserves-Card.jpg" media="(min-width: 1px)"/>
+                                        <img src="https://cdn.mahlatini.com/_900x1080_crop_center%20center_100_none/Serengeti-Grumeti-Reserves-Card.jpg" width="900" height="1080" alt="Serengeti Grumeti Reserves Card" loading="lazy"/>
+                                    </picture>
+                                </div>
+                                <div className="image-grid__card-title">
+                                    <h3>SERENGETI</h3>
+                                    <strong></strong>
+                                </div>
+                            </figure>
+                        </a>
+                    </div>
+                    <div className="col-6 col-md-4">
+                        <a href="https://www.mahlatini.com/zimbabwe/victoria-falls" className="image-grid__card">
+                            <figure>
+                                <div className="nooverflow h-96 w-96">
+                                    <picture><source type="image/webp" srcset="https://cdn.mahlatini.com/_900x1080_crop_center%20center_100_none/7713/Victoria-Falls-Card-x2.webp" media="(min-width: 768px)"/>
+                                        <source type="image/jpg" srcset="https://cdn.mahlatini.com/_900x1080_crop_center%20center_100_none/Victoria-Falls-Card-x2.jpg" media="(min-width: 768px)"/>
+                                        <source type="image/webp" srcset="https://cdn.mahlatini.com/_302x362_crop_center%20center_100_none/7713/Victoria-Falls-Card-x2.webp" media="(min-width: 1px)"/>
+                                        <source type="image/jpg" srcset="https://cdn.mahlatini.com/_302x362_crop_center%20center_100_none/Victoria-Falls-Card-x2.jpg" media="(min-width: 1px)"/>
+                                        <img src="https://cdn.mahlatini.com/_900x1080_crop_center%20center_100_none/Victoria-Falls-Card-x2.jpg" width="900" height="1080" alt="Victoria Falls Card x2" loading="lazy"/>
+                                    </picture>
+                                </div>
+                                <div className="image-grid__card-title">
+                                    <h3>VICTORIA FALLS</h3>
+                                    <strong></strong>
+                                </div>
+                            </figure>
+                        </a>
+                    </div>
+                </div>
+                <div className="pt-25 pt-md-30">
+                    <a href="https://www.mahlatini.com/destinations" className="button button--green-light">
+                        ALL DESTINATIONS</a>
+                </div>
+            </div>
+        </section>
+
+    </>
+    );
+}
+
+export default Testimonial;
